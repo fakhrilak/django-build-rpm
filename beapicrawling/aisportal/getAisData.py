1ee938dd4e1998f1fc205b2330f7bed10023e2d2
root <root@middleware-internal.tnial.go.id>
2022-09-19 22:15:08 +0700

    y

--- beapicrawling/aisportal/getAisData.py
+++ beapicrawling/aisportal/getAisData.py
@@ -9,10 +9,8 @@ class getDataFromApiAis():
     
     def requestAIS(self):
         try:
-            print(self.endPoint,"inininiin")
-            res = requests.get(url = "http://api.tnial.mil.id/home/sit/ops/ais/675cc2ce24cecd94f8241f98a1b2e3e5fec9e99ba4f97f8106a42cf67da3ddc18848c0402b29b8f0fd3915bcd38837a8a0958f4be39a9f25dbbf9e4337f83ba8/",
+            res = requests.request("GET",url = "http://api.tnial.mil.id/home/sit/ops/ais/675cc2ce24cecd94f8241f98a1b2e3e5fec9e99ba4f97f8106a42cf67da3ddc18848c0402b29b8f0fd3915bcd38837a8a0958f4be39a9f25dbbf9e4337f83ba8/",
             headers={},data={},verify=False)
-            time.sleep(5)
             print(res.text," ========== ini")
             # print(res.status_code,"ini")
             return res.text
